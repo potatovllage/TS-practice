2c9ac354435188f98b6ec793b7465669076951fe
Yeong joon <bluehome8626@naver.com>
2022-01-05 21:36:36 +0900

    ⚡️ :: MakePage axios 및 기능 추가

--- src/components/WritePost/MakePost.tsx
+++ src/components/WritePost/MakePost.tsx
@@ -1,20 +1,55 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as S from "./style";
+import { BASE_URL } from "../../api/export";
 
 function WritePost() {
+  const [input, setInput] = useState({
+    title: "",
+    description: "",
+  });
+
+  const { title, description } = input;
+
+  const onChange = (e: any) => {
+    const { value, name } = e.target;
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  const data = { title: title, description: description };
+
   const navigate = useNavigate();
 
-  const Main = () => {
-    navigate("/");
+  const Submit = () => {
+    axios
+      .post(BASE_URL + "/board", data)
+      .then((res) => {
+        console.log(res);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
     <S.WriteDiv>
       <S.WritePost>
-        <S.TitleInput placeholder="제목을 입력해주세요" />
-        <S.TextInput placeholder="내용을 입력해주세요" />
-        <S.Submit onClick={Main} type="button" value="제출" />
+        <S.TitleInput
+          name="title"
+          onChange={onChange}
+          placeholder="제목을 입력해주세요"
+        />
+        <S.TextInput
+          name="description"
+          onChange={onChange}
+          placeholder="내용을 입력해주세요"
+        />
+        <S.Submit onClick={Submit} type="button" value="제출" />
       </S.WritePost>
     </S.WriteDiv>
   );
