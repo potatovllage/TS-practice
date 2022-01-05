1d8ad1637a8e58867acea13b6a845526abdc1d76
Yeong joon <bluehome8626@naver.com>
2022-01-05 21:37:02 +0900

    ⚡️ :: PetchPost axios 및 기능 추가

--- src/components/WritePost/PetchPost.tsx
+++ src/components/WritePost/PetchPost.tsx
@@ -1,20 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as S from "./style";
 import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { BASE_URL } from "../../api/export";
+import axios from "axios";
 
 function PetchPost() {
-  const navigate = useNavigate();
+  const param = useParams();
 
-  const Main = () => {
-    navigate("/");
+  const [boardData, setBoardData] = useState({
+    id: "",
+    title: "",
+    description: "",
+  });
+
+  useEffect(() => {
+    axios
+      .get(BASE_URL + `/board/${param.id}`)
+      .then((res) => {
+        console.log(res.data);
+        setBoardData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const { title, description, id } = boardData;
+
+  const onChange = (e: any) => {
+    const { value, name } = e.target;
+    setBoardData({
+      ...boardData,
+      [name]: value,
+    });
   };
+
+  const data1 = { title: title, id: id };
+  const data2 = { description: description, id: id };
+
+  const PetchSubmit = () => {
+    axios
+      .patch(BASE_URL + "/board/title", data1)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    axios
+      .patch(BASE_URL + "/board/description", data2)
+      .then((res) => {
+        console.log(res);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const navigate = useNavigate();
 
   return (
     <S.PetchDiv>
       <S.PetchPost>
-        <S.TitleInput />
-        <S.TextInput />
-        <S.Submit onClick={Main} type="button" value="수정" />
+        <S.TitleInput
+          type="text"
+          name="title"
+          onChange={onChange}
+          defaultValue={boardData.title}
+        />
+        <S.TextInput
+          type="text"
+          name="description"
+          onChange={onChange}
+          defaultValue={boardData.description}
+        />
+        <S.Submit onClick={PetchSubmit} type="button" value="수정" />
       </S.PetchPost>
     </S.PetchDiv>
   );
